77cdc43b4080e3b8267b8c15dec950ff39e3b28d
Tristin Nguyen <nguye501@msu.edu>
2022-04-29 18:08:36 -0400

    starting code

---
+++ assets/game.js
@@ -0,0 +1,19 @@
+var TimeDisplay = document.querySelector(".TimeDisplay")
+var TimeLeft = 4
+
+
+function DisplayTimeLeft (){
+var Timer = setInterval(() => {
+        TimeDisplay.textContent = "Time Remaining: " + TimeLeft--
+    
+    if (TimeLeft < 0){
+        clearInterval(Timer)
+       setTimeout(() => {
+        window.location.href="Gameover.html"    
+       }, 3000);
+    } 
+
+    }, 1000);
+}
+
+DisplayTimeLeft();
