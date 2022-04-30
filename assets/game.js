dced7223fd8b6eb1c81df6ddc23faa93dec0c485
Tristin Nguyen <nguye501@msu.edu>
2022-04-30 19:37:58 -0400

    added functionality to questions

--- assets/game.js
+++ assets/game.js
@@ -1,6 +1,78 @@
 var TimeDisplay = document.querySelector(".TimeDisplay")
-var TimeLeft = 60
+var TimeLeft = 5
+var startButton = document.getElementById("start-button")
+var quizSection = document.getElementById("quiz-section")
+var answerDiv = document.createElement("div")
+var questionDiv = document.createElement("div")
+var questionIndex = 0
 
+var questionBank = [
+    {
+        Question: "Commonly used data values do not include",
+        Answers: [
+            "Boolen",
+            "Numbers and Arrays",
+            "Arrays",
+            "Loops",
+        ],
+        correctAnswer: "Loops"
+    },
+    {
+        Question: "The condition of an if else statement is enclosed with a",
+        Answers: [
+            "Parenthesis",
+            "Curly Brackets",
+            "Square Brackets",
+            "Slashes",
+        ],
+        correctAnswer: "Parenthesis"
+    },
+]
+
+var startQuiz = function(){
+    startButton.remove();
+    renderQuestion()
+}
+
+var renderQuestion = function(){
+    var questionDiv = document.createElement("div")
+    questionDiv.innerHTML = questionBank[questionIndex].question
+        quizSection.appendChild(questionDiv)
+        renderChoices()
+}
+
+var renderChoices = function() {
+    for (let i = 0; i< questionBank.answers.length; i++) {
+        answersButton.setAttribute("class", "answer-button")
+        answerButton.setAttribute("data-answers", questionBank[questionIndex].answers[i])
+        answerButton.textContent = questionBank[questionIndex].answers[0]
+        answerDiv.appendChild(answerButton)
+    }
+quizSection.appendChild(answerDiv)
+}
+var checkAnswer = function(event){
+    var nextQuestionHelper = function(){
+        questionIndex++
+        questionDiv.remove()
+        answerDiv.innerHTML = ""
+        renderQuestion()
+
+    var correct = questionBank[questionIndex].correctAnswer
+    if (questionIndex === questionBank.length-1){
+
+    }
+    else if (event.target.dataset.answer === questionBank[questionIndex].correctAnswer){
+        nextQuestionHelper()
+    }
+    else if (event.target.dataset.answer != questionBank[questionIndex].correctAnswer){
+        nextQuestionHelper
+    }
+    }
+}
+
+var answerButton = document.createElement("button")
+startButton.addEventListener("Click", startQuiz)
+answer.Div.addEventListener("Click", checkAnswer)
 
 function DisplayTimeLeft (){
 var Timer = setInterval(() => {
@@ -10,11 +82,15 @@ var Timer = setInterval(() => {
         clearInterval(Timer)
        setTimeout(() => {
         window.location.href="Gameover.html"    
-       }, 3000);
+       }, 1000);
     } 
 
     }, 1000);
 }
+
+
+
+
 
 DisplayTimeLeft();
 
